5b546711eed8350358cbfffbed36f8faba3e839f
Jorge <jorge.meza9@unmsm.edu.pe>
2020-08-16 17:06:21 -0500

    Inicio de Sistema Proveedor

---
+++ src/main/java/com/unmsm/sisprov/SisprovApplication.java
@@ -0,0 +1,13 @@
+package com.unmsm.sisprov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisprovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisprovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unmsm/sisprov/SisprovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unmsm.sisprov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisprovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
